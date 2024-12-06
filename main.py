29a40cac3019dfbf7f9a3931721dff72f0e6156e
Astrid Leal <167375135+AstridLeal@users.noreply.github.com>
2024-12-06 17:26:33 -0600

    Se agregaron comentarios

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-
-user = input('Dame tu nombre: ')
-
-print(user)
+# Código para el ejemplo de la clase 4
+import time # Importa la librería time
+user = input('Dame tu nombre: ') # Pide al usuario su nombre
+print(user) # Imprime el nombre del usuario
